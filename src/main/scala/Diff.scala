a14aa5ee54ae6c5b9709f3839e0a1b8bbf8e4083
Paul Schoenfelder <pschoenf@nerdery.com>
2013-08-07 17:09:46 -0500

    Use imperative versus recursive LCS algorithm for speed

--- src/main/scala/Diff.scala
+++ src/main/scala/Diff.scala
@@ -89,21 +89,44 @@ object Diff {
    * @param b Second string
    * @return Longest common subsequence
    */
-  private def lcs(a: String, b: String): String = {
-    if (a.size == 0 || b.size == 0) {
-      ""
-    }
-    else if (a == b) {
-      a
-    }
+  def lcs(a: String, b: String): String = {
+    if (a.size==0 || b.size==0) ""
     else {
-      if (a(a.size - 1) == b(b.size - 1)) {
-        lcs(a.substring(0, a.size - 1), b.substring(0, b.size - 1)) + a(a.size - 1)
+      if (a==b) {
+        a
       }
       else {
-        val x = lcs(a, b.substring(0, b.size - 1))
-        val y = lcs(a.substring(0, a.size - 1), b)
-        if (x.size > y.size) x else y
+        val lengths = Array.ofDim[Int](a.size+1,b.size+1)
+        for (i <- 0 until a.size) {
+          for (j <- 0 until b.size) {
+            if (a(i) == b(j)) {
+              lengths(i+1)(j+1) = lengths(i)(j) + 1
+            }
+            else {
+              lengths(i+1)(j+1) = scala.math.max(lengths(i+1)(j),lengths(i)(j+1))
+            }
+          }
+        }
+
+        // read the substring out from the matrix
+        val sb = new StringBuilder()
+        var x = a.size
+        var y = b.size
+        do {
+          if (lengths(x)(y) == lengths(x-1)(y)) {
+            x -= 1
+          }
+          else if (lengths(x)(y) == lengths(x)(y-1)) {
+            y -= 1
+          }
+          else {
+            assert(a(x-1) == b(y-1))
+            sb += a(x-1)
+            x -= 1
+            y -= 1
+          }
+        } while (x!=0 && y!=0)
+        sb.toString.reverse
       }
     }
   }
