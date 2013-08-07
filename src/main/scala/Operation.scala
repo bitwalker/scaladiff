432ac64dbe2dbe39fd445eca06d4b63bd3eb2f9c
Paul Schoenfelder <pschoenf@nerdery.com>
2013-08-07 16:54:20 -0500

    Add Operation case class

--- src/main/scala/Operation.scala
+++ src/main/scala/Operation.scala
@@ -1,16 +1,9 @@
 package net.ironforged.scaladiff
 
-/**
- * The data structure representing a diff is a LinkedList of Diff objects:
- * {
- *    Diff(Operation.Delete, "Hello"),
- *    Diff(Operation.Insert, "Goodbye"),
- *    Diff(Operation.Equal, " world.")
- * }
- * which means: delete "Hello", add "Goodbye" and keep " world."
- */
-object Operation extends Enum {
-  case object Delete extends Operation.Value; Delete
-  case object Insert extends Operation.Value; Delete
-  case object Equal  extends Operation.Value; Delete
+object OperationType extends Enum {
+  case object Insert extends OperationType.Value; Insert
+  case object Delete extends OperationType.Value; Delete
+  case object Equals extends OperationType.Value; Equals
 }
+
+case class Operation(op: OperationType.Value, text: String)
