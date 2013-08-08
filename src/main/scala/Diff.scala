030a3b670a1357e39a33de7d9e15566c00c84f72
Paul Schoenfelder <paulschoenfelder@gmail.com>
2013-08-08 09:21:08 -0500

    Beginning draft

--- src/main/scala/Diff.scala
+++ src/main/scala/Diff.scala
@@ -1,6 +1,7 @@
 package net.ironforged.scaladiff
 
 import net.ironforged.scaladiff._
+import net.ironforged.scaladiff.Commons._
 import net.ironforged.scaladiff.OperationType._
 
 case class Diff(original: String, modified: String, diffs: List[Operation]) {
@@ -34,6 +35,28 @@ case class Diff(original: String, modified: String, diffs: List[Operation]) {
       res + op
     }
   }
+
+  /**
+   * Convert the diff into a more human-readable format
+   */
+  /*private def humanize(): List[Operation] = {
+    var result = List.empty[Operation]
+
+    for (i <- 0 until diffs.indices.last) {
+      val current  = diffs(i)
+      val previous = if (diffs.indices.head == i) Operation(Equals, "") else diffs(i - 1)
+
+      current.op match {
+        case Insert => {
+          if (previous.op == Equals) {
+
+          }
+        }
+      }
+    }
+
+    result
+  }*/
 }
 
 object Diff {
@@ -127,6 +150,175 @@ object Diff {
         // Due to the traceback approach, we built the result in reverse
         builder.toString.reverse
       }
+    }
+  }
+
+  /**
+   * Reorder and merge edits, equalities. Any edit section can move as long as it doesn't cross an equality.
+   * @param diffs List of Operations
+   */
+  def cleanup(diffs: List[Operation]): String = {
+    var buffer = List(Operation(Equals, ""))
+
+    var deletes      = 0
+    var inserts      = 0
+    var commonLength = 0
+    var deleted      = ""
+    var inserted     = ""
+    var previous: Operation = null
+    var currentIndex = 0
+    var current      = diffs.head
+
+    while (current != null) {
+      current.op match {
+        case Insert => {
+          inserts   = inc(inserts)
+          inserted += current.text
+          buffer    = buffer :+ current
+          previous  = null
+        }
+        case Delete => {
+          deletes  = inc(deletes)
+          deleted += current.text
+          buffer   = buffer :+ current
+          previous = null
+        }
+        case Equals => {
+          if (deletes + inserts > 1) {
+            val bothTypes = deletes != 0 && inserts != 0
+            // Delete
+
+          }
+        }
+      }
+
+      (currentIndex, current) =
+        if (diffs.length == currentIndex) {
+          (currentIndex, null)
+        } else {
+          (inc(currentIndex), diffs(inc(currentIndex)))
+        }
+
+      case EQUAL:
+        if (count_delete + count_insert > 1) {
+          boolean both_types = count_delete != 0 && count_insert != 0;
+          // Delete the offending records.
+          pointer.previous();  // Reverse direction.
+          while (count_delete-- > 0) {
+            pointer.previous();
+            pointer.remove();
+          }
+          while (count_insert-- > 0) {
+            pointer.previous();
+            pointer.remove();
+          }
+          if (both_types) {
+            // Factor out any common prefixies.
+            commonlength = diff_commonPrefix(text_insert, text_delete);
+            if (commonlength != 0) {
+              if (pointer.hasPrevious()) {
+                thisDiff = pointer.previous();
+                assert thisDiff.operation == Operation.EQUAL
+                  : "Previous diff should have been an equality.";
+                thisDiff.text += text_insert.substring(0, commonlength);
+                pointer.next();
+              } else {
+                pointer.add(new Diff(Operation.EQUAL,
+                  text_insert.substring(0, commonlength)));
+              }
+              text_insert = text_insert.substring(commonlength);
+              text_delete = text_delete.substring(commonlength);
+            }
+            // Factor out any common suffixies.
+            commonlength = diff_commonSuffix(text_insert, text_delete);
+            if (commonlength != 0) {
+              thisDiff = pointer.next();
+              thisDiff.text = text_insert.substring(text_insert.length()
+                - commonlength) + thisDiff.text;
+              text_insert = text_insert.substring(0, text_insert.length()
+                - commonlength);
+              text_delete = text_delete.substring(0, text_delete.length()
+                - commonlength);
+              pointer.previous();
+            }
+          }
+          // Insert the merged records.
+          if (text_delete.length() != 0) {
+            pointer.add(new Diff(Operation.DELETE, text_delete));
+          }
+          if (text_insert.length() != 0) {
+            pointer.add(new Diff(Operation.INSERT, text_insert));
+          }
+          // Step forward to the equality.
+          thisDiff = pointer.hasNext() ? pointer.next() : null;
+        } else if (prevEqual != null) {
+          // Merge this equality with the previous one.
+          prevEqual.text += thisDiff.text;
+          pointer.remove();
+          thisDiff = pointer.previous();
+          pointer.next();  // Forward direction
+        }
+        count_insert = 0;
+        count_delete = 0;
+        text_delete = "";
+        text_insert = "";
+        prevEqual = thisDiff;
+        break;
+      }
+      thisDiff = pointer.hasNext() ? pointer.next() : null;
+    }
+    if (diffs.getLast().text.length() == 0) {
+      diffs.removeLast();  // Remove the dummy entry at the end.
+    }
+
+    /*
+     * Second pass: look for single edits surrounded on both sides by equalities
+     * which can be shifted sideways to eliminate an equality.
+     * e.g: A<ins>BA</ins>C -> <ins>AB</ins>AC
+     */
+    boolean changes = false;
+    // Create a new iterator at the start.
+    // (As opposed to walking the current one back.)
+    pointer = diffs.listIterator();
+    Diff prevDiff = pointer.hasNext() ? pointer.next() : null;
+    thisDiff = pointer.hasNext() ? pointer.next() : null;
+    Diff nextDiff = pointer.hasNext() ? pointer.next() : null;
+    // Intentionally ignore the first and last element (don't need checking).
+    while (nextDiff != null) {
+      if (prevDiff.operation == Operation.EQUAL &&
+        nextDiff.operation == Operation.EQUAL) {
+        // This is a single edit surrounded by equalities.
+        if (thisDiff.text.endsWith(prevDiff.text)) {
+          // Shift the edit over the previous equality.
+          thisDiff.text = prevDiff.text
+          + thisDiff.text.substring(0, thisDiff.text.length()
+            - prevDiff.text.length());
+          nextDiff.text = prevDiff.text + nextDiff.text;
+          pointer.previous(); // Walk past nextDiff.
+          pointer.previous(); // Walk past thisDiff.
+          pointer.previous(); // Walk past prevDiff.
+          pointer.remove(); // Delete prevDiff.
+          pointer.next(); // Walk past thisDiff.
+          thisDiff = pointer.next(); // Walk past nextDiff.
+          nextDiff = pointer.hasNext() ? pointer.next() : null;
+          changes = true;
+        } else if (thisDiff.text.startsWith(nextDiff.text)) {
+          // Shift the edit over the next equality.
+          prevDiff.text += nextDiff.text;
+          thisDiff.text = thisDiff.text.substring(nextDiff.text.length())
+          + nextDiff.text;
+          pointer.remove(); // Delete nextDiff.
+          nextDiff = pointer.hasNext() ? pointer.next() : null;
+          changes = true;
+        }
+      }
+      prevDiff = thisDiff;
+      thisDiff = nextDiff;
+      nextDiff = pointer.hasNext() ? pointer.next() : null;
+    }
+    // If shifts were made, the diff needs reordering and another shift sweep.
+    if (changes) {
+      diff_cleanupMerge(diffs);
     }
   }
 }
