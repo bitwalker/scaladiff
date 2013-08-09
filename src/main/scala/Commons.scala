966c18f3aab769b8eb9301a425f0c8499d8a199e
Paul Schoenfelder <paulschoenfelder@gmail.com>
2013-08-08 23:53:16 -0500

    Add replace function for lists

--- src/main/scala/Commons.scala
+++ src/main/scala/Commons.scala
@@ -29,9 +29,36 @@ object Commons {
    * @param index The index at which to insert the new element
    * @param elements The list of elements to insert
    * @tparam T The type of the list
-   * @return List[T}
+   * @return List[T]
    */
   def insert[T](xs: List[T], index: Int, elements: => List[T]): List[T] = {
     xs.take(index) ++ elements ++ xs.drop(index)
   }
+
+  /**
+   * Replace elements within a list with a single element
+   * @param xs The target list
+   * @param start The index to begin the replacement
+   * @param end The index at which to end replacment
+   * @param element The element to swap in
+   * @tparam T The type of the list
+   * @return List[T]
+   */
+  def replace[T](xs: List[T], start: Int, end: Int, element: T): List[T] = {
+    (xs.take(start) :+ element) ++ xs.drop(end)
+  }
+
+  /**
+   * Replace elements within a list with another list of elements
+   * @param xs The target list
+   * @param start The index to begin replacement
+   * @param end The index at which to end replacement
+   * @param elements The elements to swap in
+   * @tparam T The type of the list
+   * @return List[T]
+   */
+  def replace[T](xs: List[T], start: Int, end: Int, elements: List[T]): List[T] = {
+    xs.take(start) ++ elements ++ xs.drop(end)
+  }
+
 }
