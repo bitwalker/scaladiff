5bfa995e89703cbdfba125a94a9c109e8d35c75c
Paul Schoenfelder <paulschoenfelder@gmail.com>
2013-08-08 23:40:44 -0500

    Add insert function for inserting elements within a list

--- src/main/scala/Commons.scala
+++ src/main/scala/Commons.scala
@@ -3,4 +3,28 @@ package net.ironforged.scaladiff
 object Commons {
   def inc(x: Int): Int = x + 1
   def dec(x: Int): Int = x - 1
+
+  /**
+   * Insert an element within a list
+   * @param xs The target list
+   * @param index The index at which to insert the new element
+   * @param element The new element to insert
+   * @tparam T The type of the list
+   * @return List[T]
+   */
+  def insert[T](xs: List[T], index: Int, element: T): List[T] = {
+    (xs.take(index) :+ element) ++ xs.drop(index)
+  }
+
+  /**
+   * Insert multiple elements within a list
+   * @param xs The target list
+   * @param index The index at which to insert the new element
+   * @param elements The list of elements to insert
+   * @tparam T The type of the list
+   * @return List[T}
+   */
+  def insert[T](xs: List[T], index: Int, elements: => List[T]): List[T] = {
+    xs.take(index) ++ elements ++ xs.drop(index)
+  }
 }
