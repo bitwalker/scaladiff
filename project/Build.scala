2da701c03e250e1681a77e4faf6c6aeec48d2f6f
Paul Schoenfelder <paulschoenfelder@gmail.com>
2013-08-10 03:27:02 -0500

    Merge build.sbt settings into Build.scala, add ScalaTest dependency

--- project/Build.scala
+++ project/Build.scala
@@ -2,11 +2,16 @@ import sbt._
 import Keys._
 
 object Builds extends Build {
+
   lazy val buildSettings = Defaults.defaultSettings ++ Seq(
     version := "0.1",
+    name := "scaladiff",
     organization := "net.ironforged",
-    scalaVersion := "2.10.2"
+    scalaVersion := "2.10.2",
+    resolvers += "Sonatype OSS Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots",
+    libraryDependencies += "org.scalatest" % "scalatest_2.10" % "1.9.1" % "test"
   )
+
 
   lazy val app = Project(
     "scaladiff",
